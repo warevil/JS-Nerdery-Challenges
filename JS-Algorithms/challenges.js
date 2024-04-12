b0d096c0fa69abb79cc4ec9d7fae6e268b6529d9
warevil <60890246+warevil@users.noreply.github.com>
2024-04-12 17:51:17 -0500

    chore: Challenge #1

--- JS-Algorithms/challenges.js
+++ JS-Algorithms/challenges.js
@@ -13,7 +13,15 @@ Invoking "readableTime(3690)" should return "01:01:30" (HH:MM:SS)
 ***** */
 
 const readableTime = (seconds) => {
-  // YOUR CODE HERE...
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formattedHours = String(hours).padStart(2, "0");
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
 };
 
 readableTime(458);
